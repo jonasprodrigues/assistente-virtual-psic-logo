e3a01d2a697e525eb1206d332e769ce0e3e62477
jonasprodrigues <58193185+jonasprodrigues@users.noreply.github.com>
2024-04-18 11:06:16 -0300

    Atualizar o Assis.py

--- Assis.py
+++ Assis.py
@@ -1,19 +1,18 @@
 # Lista de nomes das area
 nomes_areas = ["Saúde", "Desenvolvimento Pessoal", "Carreira/Profissão", "Relacionamentos", "Finanças", "Espiritualidade", "Diversão", "Ambiente", "SAIR"]
 # Lista para armazenar os dados das áreas
-areas = []
+areas = {}
 
 # Loop para solicitar a idade dos garotos
 for nome in nomes_areas:
     nota = int(input("Digite a nota de {}: ".format(nome)))
-    #areas.append((area, nota))
     areas[nome] = nota
 
 
 # Ordenar os garotos por idade
-areas_ordenadas = sorted(areas, key=lambda x: x[1])
+areas_ordenadas = sorted(areas, key=areas.get)[:2]
 
 # Retornar os nomes das 2 áreas de menores notas
-print("As áreas de notas mais baixas são:")
-for area, _ in areas_ordenados[:2]:
-    print(area)
+print("As áreas de notas mais baixas são: ", areas_ordenadas )
+
+    
