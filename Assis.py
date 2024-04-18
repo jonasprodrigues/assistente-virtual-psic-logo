936edabfc67427183f0715e3fade092801277756
jonasprodrigues <58193185+jonasprodrigues@users.noreply.github.com>
2024-04-18 11:35:36 -0300

    Atualizar o Assis.py

--- Assis.py
+++ Assis.py
@@ -1,3 +1,5 @@
+import time
+
 # Lista de nomes das area
 nomes_areas = ["Saúde", "Desenvolvimento Pessoal", "Carreira/Profissão", "Relacionamentos", "Finanças", "Espiritualidade", "Diversão", "Ambiente", "SAIR"]
 # Lista para armazenar os dados das áreas
@@ -22,15 +24,16 @@ def opcao_areas():
 
 
 while True:
-    print """
+    texto1 =  """
 A roda da vida é uma ferramenta de autoavaliação e desenvolvimento pessoal utilizada para avaliar e visualizar diferentes áreas da vida de uma pessoa. Geralmente, essas áreas incluem aspectos como carreira, finanças, saúde, relacionamentos, desenvolvimento pessoal, lazer, espiritualidade, entre outros.
 
 A roda da vida consiste em um círculo dividido em seções, representando cada uma dessas áreas da vida. A pessoa avalia sua satisfação ou desempenho em cada área atribuindo uma pontuação, geralmente de 1 a 10, com base em sua percepção. Depois de atribuir as pontuações, a pessoa conecta os pontos no círculo, criando uma representação visual de sua situação atual.
 
 Essa representação visual ajuda a identificar áreas que estão equilibradas e áreas que precisam de mais atenção e desenvolvimento. Com base nessa análise, a pessoa pode definir metas e criar um plano de ação para melhorar as áreas que deseja aprimorar, buscando um equilíbrio mais satisfatório em sua vida.
     """
+    print(texto1)
     time.sleep(2)
-    print """
+    texto2 = """
 As 10 áreas comuns frequentemente incluídas na roda da vida:
 
 Carreira/Profissão: Satisfação e progresso na carreira ou no trabalho.
@@ -43,7 +46,7 @@ Espiritualidade: Conexão espiritual, propósito e significado na vida.
 Lazer/Recreação: Atividades de lazer, hobbies e tempo de qualidade para relaxamento.
 Ambiente: Conforto e satisfação com o ambiente físico, seja em casa ou no trabalho.
 Contribuição/Serviço: Sentido de contribuição para a comunidade, voluntariado e impacto social positivo. """
-
+    print(texto2)
     time.sleep(2)
     escolha = input("Digite 'c' para continuar ou 's' para sair: ")
     if escolha == 'c':
