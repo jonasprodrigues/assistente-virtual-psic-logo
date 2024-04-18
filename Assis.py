6d95083d36e001a14d417f987fe6040b4f19eba7
jonasprodrigues <58193185+jonasprodrigues@users.noreply.github.com>
2024-04-18 11:19:15 -0300

    Atualizar o Assis.py

--- Assis.py
+++ Assis.py
@@ -3,16 +3,33 @@ nomes_areas = ["Saúde", "Desenvolvimento Pessoal", "Carreira/Profissão", "Rela
 # Lista para armazenar os dados das áreas
 areas = {}
 
-# Loop para solicitar a idade dos garotos
-for nome in nomes_areas:
-    nota = int(input("Digite a nota de {}: ".format(nome)))
-    areas[nome] = nota
+
+def opcao_areas():
+    print("Vamos continuar então...")# Loop para 
+    #tempo de espera
+
+    #solicitar a idade dos garotos
+    for nome in nomes_areas:
+        nota = int(input("Digite a nota de {}: ".format(nome)))
+        areas[nome] = nota
 
 
-# Ordenar os garotos por idade
-areas_ordenadas = sorted(areas, key=areas.get)[:2]
+    # Ordenar os garotos por idade
+    areas_ordenadas = sorted(areas, key=areas.get)[:2]
 
-# Retornar os nomes das 2 áreas de menores notas
-print("As áreas de notas mais baixas são: ", areas_ordenadas )
+    # Retornar os nomes das 2 áreas de menores notas
+    print("As áreas de notas mais baixas são: ", areas_ordenadas )
 
+
+while True:
+    escolha = input("Digite 'c' para continuar ou 's' para sair: ")
+    if escolha == 'c':
+        print("Continuando...")
+        opcao_areas()  # Chamada da função
+    elif escolha == 's':
+        print("Saindo...")
+        break
+    else:
+        print("Opção inválida. Por favor, digite 'c' para continuar ou 's' para sair.")
+    
     
